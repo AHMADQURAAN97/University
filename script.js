d3b7c293b0cf67ed72f868df464b409332ff4623
AHMADQURAAN <>
2021-06-03 12:32:36 +0000

    added alert

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("Welcome To WebPage For University")
+
+var username =0;
+prompt ("Enter Your ID student :")
+
+console.log(username);
+
+if (username ==0){
+  alert ("HELLO")
+} else if(username >0)
+ { alert ("WELCOME")
+} else {
+alert ("HI")
+}
+
+
+document.write ("<p style=color:red > Contact Us </p>")
