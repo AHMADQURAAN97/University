f040d444a99f97460d876e63517766f5ca7fb56d
AHMADQURAAN <>
2021-06-06 19:59:45 +0000

    add files

--- script.js
+++ script.js
@@ -5,12 +5,34 @@ var username=prompt ("Enter Your ID student :")
 
 console.log(username);
 
-if (username <= 25){
-  alert("HELLO")
-} else {
-   alert("WELCOME")
+while (username > 100){
+  alert ( "username is wrong ")
+  username=prompt ("re-type your username correct")
 }
+ alert ("Welcome our students")
+
 
 
 
 document.write ("<p style=color:red > Contact Us </p>")
+ 
+
+
+
+ var x = prompt("how many times do you want to see logo unvirsity ?")
+
+var starsFun = function () {
+  var output='';
+  var i = 0;
+  while (i < x) {
+    
+   output = output +  "<img src='https://image.shutterstock.com/image-vector/king-writer-logo-template-illustration-260nw-1938777223.jpg' />"
+
+   console.log(i,output)
+
+    i++;
+  }
+
+  return output;
+
+}
